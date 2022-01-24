4f3bf06baf03b2e3afcb8a3414b498e310c7c0d3
Jorge A. Pérez <armando@multiplica.com>
2022-01-24 07:55:31 -0600

    Se agrega variable de configuracion para mostrar o cargar GraphQL Playground

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,26 @@
 import { ApolloServer } from 'apollo-server';
 import { ApolloServerPluginLandingPageGraphQLPlayground } from 'apollo-server-core';
+import { PluginDefinition } from 'apollo-server-core';
 import { context } from './context';
-
-// 1
 import { schema } from './schema';
+
+const isProd = process.env.SERVER_PROD;
+const port = process.env.SERVER_PORT;
+
+let plugins: PluginDefinition[] = [];
+
+if(isProd==='false') {
+  plugins = [ApolloServerPluginLandingPageGraphQLPlayground()];
+}
 
 export const server = new ApolloServer({
   schema,
   context,
-  plugins: [ApolloServerPluginLandingPageGraphQLPlayground()],
+  plugins,
 });
-
-const port = 3000;
 
 // 2
 server.listen({ port }).then(({ url }) => {
+  console.log('Config ==> ', isProd, plugins);
   console.log(`🚀 Server ready at ${url}`);
 });
